f3babd67a505f5b8a136cc6aa7aa301b93a03d91
Luis Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2026-06-22 01:14:33 -0600

    add logs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production'
 
+console.log("process.env: ", process.env)
+
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/creafico" : "",
